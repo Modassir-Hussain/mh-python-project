bab15bc5afebe04a53c6f91fa78699992446d8df
Modassir Hussain <modassirofficial@gmail.com>
2024-07-14 14:49:04 +0530

    new-changes

---
+++ src/main.py
@@ -0,0 +1,2 @@
+def my_func_add(a: int, b: int) -> int:
+    return a + b
---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+from src.main import my_func_add
+import pytest, math
+
+@pytest.mark.parametrize("input_1, input_2, output", [(1,2,3),(1,3,4)])
+def test_main_my_func(input_1, input_2, output,):
+    assert output == my_func_add(input_1, input_2)
+    assert output == my_func_add(input_1, input_2)
